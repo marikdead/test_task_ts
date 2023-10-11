babec676d0f14e71d1a3ee3eb0330307af3e6f48
marikdead <skvortsov_mark@mail.ru>
2023-10-11 10:59:09 +0700

    final commit
    added some comments

--- src/nomenklatura/nomenklatura.service.ts
+++ src/nomenklatura/nomenklatura.service.ts
@@ -15,14 +15,14 @@ export class NomenklaturaService {
     @InjectRepository(Nomenklature) private readonly nomenklatureRepository: Repository<Nomenklature>,
     @InjectRepository(Links) private readonly linksRepository: Repository<Links>){}
 
-  async create(createNomenklaturaDto: CreateNomenklaturaDto) {
+  async create(createNomenklaturaDto: CreateNomenklaturaDto) { // обработчик заброса на добавление новой номенклатуры (POST api/nomenklatures)
     const existNomenklature = await this.nomenklatureRepository.findOne({
       where:{
         name: createNomenklaturaDto.name,
 
       }
     })
-    if (existNomenklature) throw new BadRequestException("This item already exist")
+    if (existNomenklature) throw new BadRequestException("This item already exist") // проверка на существование номенклатуры (GET api/nomenklatures)
 
     const nomenklature = await this.nomenklatureRepository.save({
       name: createNomenklaturaDto.name,
@@ -31,35 +31,34 @@ export class NomenklaturaService {
     return {nomenklature};
   }
 
-  async findAll() {
+  async findAll() { // обработчик вывода плоского списка всей номенклатуры
     return await this.nomenklatureRepository.find();
   }
 
-  async findOne(id: number, parentQuantity: number = 1): Promise<any> {
+  async findOne(id: number, parentQuantity: number = 1): Promise<any> { // обработчик вывода всей информации о конкретной номенклатуре (GET api/nomenklatures/id)
     const nomenklature = await this.nomenklatureRepository.findOne({
         where: { id },
         relations: ["parentLinks", "parentLinks.nomenklature"],
     });
 
-    if (!nomenklature) {
+    if (!nomenklature) { // проверка на существование номенклатуры
         throw new NotFoundException(`Product with id ${id} not found`);
     }
 
     let totalCost = nomenklature.cost * parentQuantity;
     const childProducts: any[] = [];
 
-    if (nomenklature.parentLinks && nomenklature.parentLinks.length > 0) {
-        for (const link of nomenklature.parentLinks) {
+    if (nomenklature.parentLinks && nomenklature.parentLinks.length > 0) { 
+        for (const link of nomenklature.parentLinks) { // обход всех "детей" номенклатуры
             const childProduct = link.nomenklature;
             const childQuantity = link.kol;
-            const childCost = childProduct.cost * childQuantity;
             const childProductInfo = await this.findOne(childProduct.id, childQuantity);
             totalCost += childProductInfo.totalCost * parentQuantity;
             childProducts.push(childProductInfo);
         }
     }
 
-    return {
+    return { // возврат продукта с дополнительными полями
         id: nomenklature.id,
         name: nomenklature.name,
         cost: nomenklature.cost,
